e22e25c574686dc8540fb9087122e65aa91b4165
chuarib <chuarib@gmail.com>
2022-08-18 22:49:22 +0800

    linkedin icon

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { FaFacebookF, FaLinkedin } from 'react-icons/fa';
+
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
+      <div>
+        <FaLinkedin />
+    </div>
     <div>
         <BsTwitter />
     </div>
